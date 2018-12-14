4d14299f0489cbcadf46a4f431bf75866e095933
Mahmoud <m.moghni99@gmail.com>
2018-12-14 17:01:27 +0200

    Create HotelReservation Solution

---
+++ HotelReservation.Core/Entities/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelReservation.Core.Entities
+{
+    public class Room
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public int MaxNumOfAdults { get; set; }
+        public int MaxNumOfChildren { get; set; }
+    }
+}
