f34e14459bdc4b6e8b05bd8ee31d48c94e82c727
Mahmoud <m.moghni99@gmail.com>
2018-12-15 03:14:01 +0200

    Add Meals controller and its views

---
+++ HotelReservation.Web.Admin/Controllers/MealsController.cs
@@ -0,0 +1,115 @@
+﻿namespace HotelReservation.Web.Admin.Controllers
+{
+    using HotelReservation.Core.Contracts;
+    using HotelReservation.Core.Entities;
+    using HoteReservation.Web.Models.Meal;
+    using HoteReservation.Web.Models.Room;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    public class MealsController : Controller
+    {
+        public readonly IMealRepository _mealRepository;
+
+        public MealsController(IMealRepository mealRepository)
+        {
+            _mealRepository = mealRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var meals = _mealRepository.GetAllMeals();
+
+            List<MealViewModel> mealsOutputModel = new List<MealViewModel>();
+
+            foreach (var meal in meals)
+            {
+                mealsOutputModel.Add(new MealViewModel()
+                {
+                    Id = meal.Id,
+                    Type = meal.Type,
+                });
+            }
+
+            return View(mealsOutputModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MealInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var meal = Meal.New(model.Type);
+
+                _mealRepository.AddMeal(meal);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var meal = _mealRepository.GetMealById(id);
+
+            var mealOutputModel = new MealViewModel()
+            {
+                Id = meal.Id,
+                Type = meal.Type,
+            };
+
+            return View(mealOutputModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, MealViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var meal = _mealRepository.GetMealById(id);
+
+                meal.Update(model.Type);
+
+                _mealRepository.UpdateMeal();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        // GET: Rooms/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
