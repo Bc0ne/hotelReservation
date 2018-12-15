9c1b1bef772d361df745298afa62b65adeef6469
Mahmoud <m.moghni99@gmail.com>
2018-12-15 03:13:05 +0200

    Add Dependency Resolver

---
+++ HotelReservation.Web.Admin/Bootstraper/DependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿namespace HotelReservation.Web.Admin.Bootstraper
+{
+    using Autofac;
+    using HotelReservation.Data.Context;
+    using HotelReservation.Data.Repositories;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class DependencyResolver : Module
+    {
+        private readonly IHostingEnvironment _env;
+
+        public DependencyResolver(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            LoadModules(builder);
+        }
+
+        private void LoadModules(ContainerBuilder builder)
+        {
+            builder.RegisterType<ReservationContext>().InstancePerLifetimeScope();
+
+            builder.RegisterType<RoomRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<MealRepository>().AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
